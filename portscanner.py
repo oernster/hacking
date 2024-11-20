b8abf39d7b28f7c9af0ef2bd0d85b9124ab50098
Oliver Ernster <oliverjernster@hotmail.com>
2024-11-20 13:55:20 +0000

    Complete rewrite to make it actually work!!!

--- portscanner.py
+++ portscanner.py
@@ -1,34 +1,40 @@
 import optparse
 from socket import *
 from threading import *
+import threading
+import time
 
 screenLock = Semaphore(value=1)
+socketLock = Semaphore(value=2)
+
 
 class Scanner(object):
     def __init__(self):
         self.runner()
 
-    def connScan(self):
+    def connScan(self, tgtHost, tgtPort):
+        socketLock.acquire()
         try:
             connSkt = socket(AF_INET, SOCK_STREAM)
-            connSkt.connect((self.tgtHost, self.tgtPort))
+            connSkt.connect((tgtHost, tgtPort))
             connSkt.send('Attack vector initialisation!!!\r\n')
             results = connSkt.recv(100)
             screenLock.acquire()
-            print(f"[+]{self.tgtPort}/tcp open")
+            print(f"[+]{tgtPort}/tcp open")
             print(f"[+] {str(results)}")
         except:
             screenLock.acquire()
-            print("[-]{self.tgtPort}/tcp closed")
+            print(f"[-]{tgtPort}/tcp closed")
         finally:
             screenLock.release()
             connSkt.close()
+            socketLock.release()
     
     def portScan(self, tgtHost, tgtPorts):
         try:
-            tgtIP = gethostbyname(self.tgtHost)
+            tgtIP = gethostbyname(tgtHost)
         except:
-            print(f"[-] Cannot resolve '{self.tgtHost}': Unknown host")
+            print(f"[-] Cannot resolve '{tgtHost}': Unknown host")
             return
         
         try:
@@ -36,26 +42,34 @@ class Scanner(object):
             print(f"\n[+] Scan results for: {tgtName[0]}")
         except:
             print(f"\n[+] Scan results for: {tgtIP}")
-
-        setdefaulttimeout(1)
-        for self.tgtPort in self.tgtPorts:
-            t = Thread(target=self.connScan, args=(self.tgtHost, int(self.tgtPort)))
+        setdefaulttimeout(5)
+        for tgtPort in tgtPorts:
+            while threading.active_count() > 100 :
+                time.sleep(1)
+            t = Thread(target=self.connScan, args=(tgtHost, int(tgtPort)))
             t.start()
 
     def runner(self):
         parser = optparse.OptionParser('usage%prog '+\
                                        '-H <target host> -p <target_port>')
-        parser.add_option('-H', dest='self.tgtHost', type='string', \
+        parser.add_option('-H', dest='tgtHost', type='string', \
                           help='specify target host')
-        parser.add_option('-p', dest='self.tgtPort', type='string', \
+        parser.add_option('-p', dest='tgtPort', type='string', \
                           help='specify target port[s] separated by comma')
         (options, args) = parser.parse_args()
-        self.tgtHost = options.tgtHost
-        self.tgtPorts = str(options.tgtPort).split(',')
-        if (self.tgtHost == None) | (self.tgtPorts[0] == None):
+        tgtHost = options.tgtHost
+        tgtPorts = (str(options.tgtPort)).split(',')
+        if (tgtHost == None) | (tgtPorts[0] == 'None'):
             print(parser.usage)
             exit(0)
-        self.portScan(self.tgtHost, self.tgtPorts)
+        print(tgtPorts)
+        if tgtPorts[0] == 'ALL':
+            tgtPorts_ALL = []
+            for i in range(1, 65537):
+                tgtPorts_ALL.append(i)
+            self.portScan(tgtHost, tgtPorts_ALL)
+        else:
+            self.portScan(tgtHost, tgtPorts)
 
 
 if __name__ == '__main__':
